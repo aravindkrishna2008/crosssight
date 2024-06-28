728e3b5ee6a20155b2ed7e2b06e700f39fd93a58
aravindkrishna2008 <86750995+aravindkrishna2008@users.noreply.github.com>
2024-06-27 21:30:57 -0700

    swift stuff

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from 'expo-router';
+import { Tabs, Stack } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
@@ -9,21 +9,13 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
+    <Stack>
+      <Stack.Screen
         name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
+        // hide the header for the tab navigator
+        options={{ headerShown: false }}
       />
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
@@ -31,7 +23,7 @@ export default function TabLayout() {
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
         }}
-      />
-    </Tabs>
+      /> */}
+    </Stack>
   );
 }
---
+++ crosssight/crosssight/crosssightApp.swift
@@ -0,0 +1,17 @@
+//
+//  crosssightApp.swift
+//  crosssight
+//
+//  Created by Aravind Arivudainambi on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct crosssightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
