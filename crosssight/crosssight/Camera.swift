ba59ec7b1b1f59e88b6562cf415228403e1d033c
aravindkrishna2008 <86750995+aravindkrishna2008@users.noreply.github.com>
2024-06-28 20:37:11 -0700

    bye swift

---
+++ crosssight/crosssight/Camera.swift
@@ -0,0 +1,8 @@
+//
+//  Camera.swift
+//  crosssight
+//
+//  Created by Aravind Arivudainambi on 6/27/24.
+//
+
+import Foundation
---
+++ crosssight/crosssight/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  crosssight
+//
+//  Created by Aravind Arivudainambi on 6/27/24.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
